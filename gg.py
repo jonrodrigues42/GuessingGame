e2fa8687732bb2efca1ba8b4171b7ed9697f7afd
Jon Rodrigues <joao.nrr@gmail.com>
2023-03-28 00:21:09 -0300

    bug fixes and errors handling

--- gg.py
+++ gg.py
@@ -5,22 +5,27 @@ def main_game():
     attemps = 1
     min = 0
 
-    dif = int(input(""""Escolha a dificuldade\n
+    difin = (input(""""Escolha a dificuldade\n
         0 = Easy (0 - 10)
         1 = Normal (0 - 20)
         2 = Hard (0 - 50)
         3 = Insane (0 - 100)\n"""))
     
-    if dif == 0:
-        max = 10
-    elif dif == 1:
-        max = 20
-    elif dif == 2:
-        max = 50
-    elif dif == 3:
-        max = 100
+    if difin.isdigit():
+        dif = int(difin)
+        if dif == 0:
+            max = 10
+        elif dif == 1:
+            max = 20
+        elif dif == 2:
+            max = 50
+        elif dif == 3:
+            max = 100
+        else:
+            print("Difculdade inválida\n\n\n")
+            main_game()
     else:
-        print("Difculdade inválida")
+        print("Entrada inválida")
         main_game()
 
     print(f"Será sorteado um número de {min} a {max}. Você consegue acertar?")
@@ -28,25 +33,30 @@ def main_game():
     generated_number = rd.randint(min, max)
 
     while not win:
-        usern = int(input("\nQual é o número? "))
-        if (usern >= min) and (usern <= max):
-            if generated_number == usern:
-                print("\nParabéns, você ganhou!")
-                print(f"Você acertou o número '{generated_number}' em {attemps} tentativas.\n")
-                win = True
-            elif generated_number < usern:
-                print("Ainda não, o número é MENOR")
-                attemps += 1
-            elif generated_number > usern:
-                print("Ainda não, o número é MAIOR")
-                attemps += 1
+        usernin = input("\nQual é o número? ")
+        if usernin.isdigit():
+            usern = int(usernin)
+            if (usern >= min) and (usern <= max):
+                if generated_number == usern:
+                    print("\nParabéns, você ganhou!")
+                    print(f"Você acertou o número '{generated_number}' em {attemps} tentativas.\n")
+                    win = True
+                elif generated_number < usern:
+                    print("Ainda não, o número é MENOR")
+                    attemps += 1
+                elif generated_number > usern:
+                    print("Ainda não, o número é MAIOR")
+                    attemps += 1
+            else:
+                print("Número inválido")
         else:
-            print("Número inválido")
+            print("Entrada inválida\n\n\n")
     
     again = input("\n\nJogar novamente? (S/N) ")
     if again.lower() == 's':
         main_game()
     else:
         print("Até mais!")
+# end main game
     
 main_game()
