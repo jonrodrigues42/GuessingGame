2e8fde81d937573a9d4ffdf79867d1b0e6a0fbe8
Jon Rodrigues <joao.nrr@gmail.com>
2023-03-27 23:56:39 -0300

    implemented the play again function

--- gg.py
+++ gg.py
@@ -2,34 +2,37 @@ import random as rd
 
 min = 0
 max = 10
-generated_number = rd.randint(min, max)
-print("Será sorteado um número de 0 a 10. Você consegue acertar?")
-print("---------------------------------")
 
 
 
-
-
-# usernum = int(input("Qual é o número?"))
-def main_game(n):
+def main_game(min, max):
     attemps = 1
     win = False
 
+    print(f"Será sorteado um número de {min} a {max}. Você consegue acertar?")
+    print("---------------------------------\n\n")
+    generated_number = rd.randint(min, max)
+
     while not win:
-        usern = int(input("Qual é o número?" ))
+        usern = int(input("Qual é o número? "))
         if (usern >= min) and (usern <= max):
-            if n == usern:
+            if generated_number == usern:
                 print("Parabéns, você ganhou!")
-                print(f"Você acertou o número {n} em {attemps} tentativas.")
+                print(f"Você acertou o número '{generated_number}' em {attemps} tentativas.")
                 win = True
-            elif n < usern:
+            elif generated_number < usern:
                 print("Ainda não, o número é MENOR")
                 attemps += 1
-            elif n > usern:
+            elif generated_number > usern:
                 print("Ainda não, o número é MAIOR")
                 attemps += 1
         else:
             print("Número inválido")
     
 
-main_game(generated_number)
+main_game(min, max)
+again = input("Jogar novamente? (S/N) ")
+if again.lower() == 's':
+    main_game(min, max)
+else:
+    print("Até mais!")
